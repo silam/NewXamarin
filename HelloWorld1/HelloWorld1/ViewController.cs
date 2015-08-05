f4d2b5d5affae10f472c1a276595ff412941d844
Si Lam <silam@hotmail.com>
2015-08-05 15:54:08 -0500

    button

--- HelloWorld1/HelloWorld1/ViewController.cs
+++ HelloWorld1/HelloWorld1/ViewController.cs
@@ -14,12 +14,21 @@ namespace HelloWorld1
 		{
 			base.ViewDidLoad ();
 			// Perform any additional setup after loading the view, typically from a nib.
+			int count = 0;
+
+			button.TouchUpInside +=	(sender, e) => this.labelcount.Text   = string.Format ("Count: {0}", ++count);
+
 		}
 
 		public override void DidReceiveMemoryWarning ()
 		{
 			base.DidReceiveMemoryWarning ();
 			// Release any cached data, images, etc that aren't in use.
+		}
+
+		partial void UIButton4_TouchUpInside (UIButton sender)
+		{
+			throw new NotImplementedException ();
 		}
 	}
 }
--- HelloWorld1/HelloWorld1/ViewController.designer.cs
+++ HelloWorld1/HelloWorld1/ViewController.designer.cs
@@ -14,8 +14,36 @@ namespace HelloWorld1
 	[Register ("ViewController")]
 	partial class ViewController
 	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton button { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIView label { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel labelcount { get; set; }
+
+		[Action ("UIButton4_TouchUpInside:")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void UIButton4_TouchUpInside (UIButton sender);
+
 		void ReleaseDesignerOutlets ()
 		{
+			if (button != null) {
+				button.Dispose ();
+				button = null;
+			}
+			if (label != null) {
+				label.Dispose ();
+				label = null;
+			}
+			if (labelcount != null) {
+				labelcount.Dispose ();
+				labelcount = null;
+			}
 		}
 	}
 }
